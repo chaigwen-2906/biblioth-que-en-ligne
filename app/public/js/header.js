2790f0a3852b725ff5b4310ed0636feecc8c406b
LEMOINE Gwenola <chaigwen29.06@gmail.com>
2020-03-15 11:12:49 +0100

    boiteModalCreerCompte

--- app/public/js/header.js
+++ app/public/js/header.js
@@ -22,6 +22,7 @@ $(".btnloupe").click(function(){
 
 //------------------ Modal Connection ---------------//
 let boutonSidentifier = document.getElementById("boutonSidentifier");
+
 boutonSidentifier.addEventListener("click",function(e) {
     //On désactive le comportement du lien
     e.preventDefault();
@@ -38,3 +39,50 @@ boutonFermerModalConnection.addEventListener("click",function(e) {
     //On affiche le modalConnection
     $("#modalConnection").hide("slow");
 });
+
+//------------------ Modal Creer Compte ---------------//
+let boutonCreerCompte = document.getElementById("boutonCreerCompte");
+boutonCreerCompte.addEventListener("click",function(e) {
+    //On désactive le comportement du lien
+    e.preventDefault();
+
+    //On affiche le modalConnection
+    $("#modalCreerCompte").show("slow");
+});
+
+let fermerModalCreerCompte = document.getElementById("fermerModalCreerCompte");
+fermerModalCreerCompte.addEventListener("click",function(e) {
+    //On désactive le comportement du lien
+    e.preventDefault();
+
+    //On affiche le modalConnection
+    $("#modalCreerCompte").hide("slow");
+});
+//------------------FIN  Modal Creer Compte ---------------//
+
+//--------------------MODAL FAQ---------------------------//
+let boutonFaq = document.getElementById("boutonFaq");
+boutonFaq.addEventListener("click",function(e) {
+    //On désactive le comportement du lien
+    e.preventDefault();
+
+    //On affiche le modalConnection
+    $("#modalFaq").show("slow");
+});
+
+let fermerModalFaq = document.getElementById("fermerModalFaq");
+fermerModalFaq.addEventListener("click",function(e) {
+    //On désactive le comportement du lien
+    e.preventDefault();
+
+    //On affiche le modalConnection
+    $("#modalFaq").hide("slow");
+});
+
+
+
+
+
+
+
+//---------------FIN MODAL FAQ---------------------------//
