85105b1f1fd6a5f7d347874f26efd499f2d67360
LEMOINE Gwenola <chaigwen29.06@gmail.com>
2020-04-09 12:38:06 +0200

    gestion-panier-responsiv-detailLivre

--- app/models/ManagerFrontPanier.php
+++ app/models/ManagerFrontPanier.php
@@ -26,14 +26,15 @@ class ManagerFrontPanier extends Manager
         return $resultat;
     }
 
-    public function getDemandeEnAttente($idClient)
+    public function getListDemandeEnAttente($idClient)
     {
         
         $bdd = $this->dbConnect();
 
         //On réalise la requete sur la base de données
         //On prépare la requete
-        $sql = "SELECT * FROM atelier WHERE idClient";
+        $sql = "SELECT * FROM reservation LEFT JOIN (livre LEFT JOIN auteur ON (livre.idAuteur = auteur.idAuteur)) 
+        ON (reservation.idLivre = livre.idLivre)  WHERE idClient = $idClient AND statut LIKE 'En attente de validation'  ORDER BY reservation.dateDeDebut DESC  ";
         $requete = $bdd->prepare($sql);
 
         //Execution de la requete
@@ -47,5 +48,46 @@ class ManagerFrontPanier extends Manager
 
         //On retourne les résultats
         return $resultat;
+    }
+
+
+    public function getListDemandeValider($idClient)
+    {
+        $bdd = $this->dbConnect();
+
+        //On réalise la requete sur la base de données
+        //On prépare la requete
+        $sql = "SELECT * FROM reservation LEFT JOIN (livre LEFT JOIN auteur ON (livre.idAuteur = auteur.idAuteur)) 
+        ON (reservation.idLivre = livre.idLivre)  WHERE idClient = $idClient AND statut LIKE 'Validée'  ORDER BY reservation.dateDeDebut DESC  ";
+        $requete = $bdd->prepare($sql);
+
+        //Execution de la requete
+        $requete->execute();
+
+        //On récupère le résultat de la requete
+        $resultat = $requete->fetchAll();
+
+        //On ferme la requete
+        $requete->closeCursor();
+
+        //On retourne les résultats
+        return $resultat;
+    }
+
+    public function ajoutReservation($idLivre,$idClient)
+    {
+        $bdd = $this->dbConnect();
+
+        //On réalise la requete sur la base de données
+        //On prépare la requete
+        $sql = " INSERT INTO reservation(idClient,idLivre,dateDeDebut,statut) VALUES (?,?,NOW(),'En attente de validation') ";
+        $requete = $bdd->prepare($sql);
+
+        //Execution de la requete
+        $requete->execute([$idClient,$idLivre]);
+
+        //On ferme la requete
+        $requete->closeCursor();
+
     }
 }
